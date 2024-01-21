dd21bb379cbf96743beb4fc302a5f5dc99c0d8d9
Dustin Fields <admin@stormycloud.org>
2024-01-20 22:24:40 -0600

    changing font

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>I2P Proxy Check</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://rsms.me/">
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap" rel="stylesheet">
+    
     <!-- ... [Defines Crypto Copy to Clipboard function] ... -->
     <script>
         function copyToClipboard(text, elementId) {
