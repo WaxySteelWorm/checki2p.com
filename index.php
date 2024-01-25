3c27c84fe09107ceb684e0c082f639bd2ad341e9
StormyCloudInc <64628149+WaxySteelWorm@users.noreply.github.com>
2024-01-25 07:05:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="preload" href="/assets/fonts/InterVariable.woff2" as="font" type="font/woff2" crossorigin=anonymous>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>I2P Proxy Check</title>
