6e928a6080983e947b46b69d6961f40902727fc7
Dustin <dfields@stormycloud.org>
2024-01-29 21:29:37 -0600

    dottttsssss

--- reseed/index.php
+++ reseed/index.php
@@ -41,34 +41,34 @@
         <span id="btc-confirmation" class="copy-confirmation">Copied!</span>
     </div>
     <div class="message">
-        <?php
-        include 'db.php';
+    <?php
+    include 'db.php';
 
-        $result = $conn->query("SELECT * FROM server_status");
-        $last_checked_global = "";
+    $result = $conn->query("SELECT * FROM server_status");
+    $last_checked_global = "";
 
-        echo "<table class='status-table'>";
-        echo "<tr><th>Server Name</th><th>Status</th></tr>";
+    echo "<table class='status-table'>";
+    echo "<tr><th>Server Name</th><th>Status</th></tr>";
 
-        while($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["server_name"] . "</td>";
-            if ($row["status"] == "offline") {
-                echo "<td><img src='../assets/images/offline.svg' alt='Offline' class='status-icon'> OFFLINE since " . $row["last_checked"] . "</td>";
-            } else {
-                echo "<td><img src='../assets/images/online.svg' alt='Online' class='status-icon'> ONLINE</td>";
-            }
-            echo "</tr>";
-            $last_checked_global = $row["last_checked"];
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["server_name"] . "</td>";
+        if ($row["status"] == "offline") {
+            echo "<td><span class='dot glowing-red'></span> OFFLINE since " . $row["last_checked"] . "</td>";
+        } else {
+            echo "<td><span class='dot glowing-green'></span> ONLINE</td>";
         }
+        echo "</tr>";
+        $last_checked_global = $row["last_checked"];
+    }
 
-        echo "</table>";
+    echo "</table>";
 
-        echo "<p class='last-checked'>Last Checked: " . $last_checked_global . "</p>";
+    echo "<p class='last-checked'>Last Checked: " . $last_checked_global . "</p>";
 
-        $conn->close();
-        ?>
-    </div>
+    $conn->close();
+    ?>
+</div>
     <footer>
     Created by <a href=https://stormycloud.org/>stormycloud.org</a> | <a href="https://github.com/WaxySteelWorm/checki2p.com" target="_blank">Github</a> | <a href="https://twitter.com/StormyCloudInc" target="_blank">Twitter</a> | <a href="https://www.instagram.com/stormycloudinc/" target="_blank">Instagram</a>    </footer>
     </body>
