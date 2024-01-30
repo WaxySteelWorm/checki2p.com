d7edcf9e2ed574fbb3d63a100ddc55f3589eab7c
Dustin <dfields@stormycloud.org>
2024-01-30 09:27:25 -0600

    removed footer

--- reseed/index.php
+++ reseed/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>I2P Reseed Check</title>
-    <link rel="stylesheet" href="/assets/css/merged_style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicon.ico">
 </head>
 <body>
@@ -59,9 +59,7 @@ $conn->close();
             </div>
         </div>
 
-        <footer>
-        Created by <a href=https://stormycloud.org/>stormycloud.org</a> | <a href="https://github.com/WaxySteelWorm/checki2p.com" target="_blank">Github</a> | <a href="https://twitter.com/StormyCloudInc" target="_blank">Twitter</a> | <a href="https://www.instagram.com/stormycloudinc/" target="_blank">Instagram</a>    </footer>
-        </footer>
+
     </div>
 </body>
 </html>
