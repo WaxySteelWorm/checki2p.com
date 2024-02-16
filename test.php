2b46c0637f9f9f8b75fd53d59345debc07eafb02
Dustin <dfields@stormycloud.org>
2024-02-16 08:42:26 -0600

    test

--- test.php
+++ test.php
@@ -8,14 +8,21 @@
 </style>
 </head>
 <body>
-<div class="donate-banner">
-    Please consider donating:
-    <button onclick="copyToClipboard('45Gtj5tkhs4EsbnV7kkhMCRpbZUdqCQqR5qmLFVLAvbFCYaPL4pFbBkEBLJ7beHqkiJxdTBkPwFsT5EMu5jDrYBHPjQzPuv', 'xmr-confirmation')">Copy XMR Address</button>
-    <button onclick="copyToClipboard('LULvg4mJc9Y37hU3sbVTMxJAPyNwyCJHw6', 'ltc-confirmation')">Copy ETH Address</button>
-    <button onclick="copyToClipboard('1NDRuQNyJZmYK4AJwve1aqa56ntuXpbyA7', 'btc-confirmation')">Copy BTC Address</button>
-    <span id="xmr-confirmation" class="copy-confirmation">Copied!</span>
-    <span id="ltc-confirmation" class="copy-confirmation">Copied!</span>
-    <span id="btc-confirmation" class="copy-confirmation">Copied!</span>
+<!-- Trigger/Open The Modal -->
+<button id="myBtn">Donate XMR</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Click the button below to copy the XMR address:</p>
+    <button onclick="copyToClipboard('YourXMRAddress', 'modal-xmr-confirmation')">Copy XMR Address</button>
+    <span id="modal-xmr-confirmation" class="copy-confirmation">Copied!</span>
+  </div>
+
 </div>
+
 </body>
 </html>
