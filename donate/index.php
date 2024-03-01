cb8adc779825ca5bd8fdec110f099103979443b5
Dustin <dfields@stormycloud.org>
2024-03-01 14:50:15 -0600

    css

--- donate/index.php
+++ donate/index.php
@@ -50,7 +50,7 @@
     <h2 class="donation-title">Support CheckI2P.com</h2>
     <p class="donation-text">CheckI2P.com is proudly developed and maintained by <a href="https://www.stormycloud.org" class="bold-link">StormyCloud Inc.</a> Your donation will contribute to the ongoing operations of StormyCloud Inc. With community support, we aim to expand our influence within the privacy-focused community.</p>
     <p class="donation-text">All donations are directed towards the operating expenses of StormyCloud Inc and are exclusively used to fund initiatives centered on privacy.</p>
-
+    </div>
 
     <table id="donate_options">
 <thead>
@@ -77,7 +77,7 @@
 </tbody>
 </table>
 </div>
-</div>
+
     <footer>
     Created by <a href=https://stormycloud.org/>stormycloud.org</a> | <a href="https://github.com/WaxySteelWorm/checki2p.com" target="_blank">Github</a> | <a href="https://twitter.com/StormyCloudInc" target="_blank">Twitter</a> | <a href="https://www.instagram.com/stormycloudinc/" target="_blank">Instagram</a>    </footer>
     </body>
