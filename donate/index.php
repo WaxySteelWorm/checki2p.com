82ac353d6699ba76f3b42a43f735e9975a447c16
Dustin <dfields@stormycloud.org>
2024-03-01 13:56:09 -0600

    testing blank banner

--- donate/index.php
+++ donate/index.php
@@ -44,13 +44,7 @@
         <img src="../assets/images/logo.svg" alt="Logo" class="logo">
     </div>
     <div class="donate-banner">
-        Please consider donating:
-        <span onclick="copyToClipboard('45Gtj5tkhs4EsbnV7kkhMCRpbZUdqCQqR5qmLFVLAvbFCYaPL4pFbBkEBLJ7beHqkiJxdTBkPwFsT5EMu5jDrYBHPjQzPuv', 'xmr-confirmation')">XMR</span> |  <span onclick="copyToClipboard('LULvg4mJc9Y37hU3sbVTMxJAPyNwyCJHw6', 'ltc-confirmation')">ETH</span> |
-       
-        <span onclick="copyToClipboard('1NDRuQNyJZmYK4AJwve1aqa56ntuXpbyA7', 'btc-confirmation')">BTC</span>
-        <span id="xmr-confirmation" class="copy-confirmation">Copied!</span>
-        <span id="ltc-confirmation" class="copy-confirmation">Copied!</span>
-        <span id="btc-confirmation" class="copy-confirmation">Copied!</span>
+
     </div>
     <div class="message">
     <table id="donate_options">
